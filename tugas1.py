5e4de2aa8dce27c09962e31e3d7c8d2926db5549
khalid0931 <mutiarif53@gmail.com>
2026-09-23 15:48:10 +0700

    membuat kode tugas 1

---
+++ tugas1.py
@@ -0,0 +1,12 @@
+grade = int(input("masukkan grade: "))
+
+if grade >= 90:
+    print("Excellent performance")
+elif grade >= 80:
+    print("Very Good performance")
+elif grade >= 70:
+    print("Good performance")
+elif grade >= 60:
+    print("Average performance")
+else:
+    print("poor performance")
